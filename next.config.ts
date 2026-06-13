456b43abc978aeb01481a8fb587f8b1d95611d30
Ndivhuwo Munyai <zwivhuyaex@gmail.com>
2026-06-13 23:10:18 +0200

    chore: add server external packages to Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverExternalPackages: ['genkit', '@genkit-ai/core', '@opentelemetry/sdk-node', 'handlebars', '@opentelemetry/exporter-jaeger'],
   typescript: {
     ignoreBuildErrors: true,
   },
